731ad75484060dc211cdc97f2c07bccb56c62f53
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-12-14 20:38:34 +0530

    Update dice.py

--- plugins/helpers/dice.py
+++ plugins/helpers/dice.py
@@ -15,7 +15,7 @@ DICE_E_MOJI = "🎲"
     f_onw_fliter
 )
 async def roll_dice(client, message):
-    """ @RollaDie """
+    """ @MOVIES_VILLA_UPDATE """
     rep_mesg_id = message.id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.id
